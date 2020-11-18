40f1a84661c92fc7ddd6edff9a72c24f4b20beec
DavidM222555 <46248699+DavidM222555@users.noreply.github.com>
2020-11-18 16:09:40 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+import SQLWrapper
+
+# Purpose: Validates a password for a given user
+def validate_password(database_file, username, entered_password):
+    actualPassword = SQLWrapper.get_password_for_user(database_file, username)
+
+    if(actualPassword != entered_password):
+        return False
+    else:
+        return True
+

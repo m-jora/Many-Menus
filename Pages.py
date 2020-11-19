ac7f6c330441c12134395e0a2b4cc719113aacc8
DavidM222555 <46248699+DavidM222555@users.noreply.github.com>
2020-11-19 12:25:49 -0600

    Add files via upload

---
+++ Pages.py
@@ -0,0 +1,30 @@
+class RestaurantCreateAccount:
+    def __init__(self):
+        return
+
+class CustomerCreateAccount:
+    def __init__(self):
+        return
+
+class RestaurantUpdateInfo:
+    def __init__(self):
+        return
+
+class RestaurantUpdateInventory:
+    def __init__(self):
+        return
+
+class RestaurantUpdateMenu:
+    def __init__(self):
+        return
+
+class UpdateUserInfo:
+    def __init__(self):
+        return
+
+class Login:
+    def __init__(self):
+        return
+
+
+ 

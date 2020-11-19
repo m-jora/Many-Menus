cdd5eac5a8f3832959d61c570e5484694180759c
DavidM222555 <46248699+DavidM222555@users.noreply.github.com>
2020-11-19 12:30:00 -0600

    Update Pages.py

--- Pages.py
+++ Pages.py
@@ -31,3 +31,12 @@ class Browse:
     def __init__(self):
         return 
  
+
+# Purpose: Validates a password for a given user
+def validate_password(database_file, username, entered_password):
+    actualPassword = SQLWrapper.get_password_for_user(database_file, username)
+
+    if(actualPassword != entered_password):
+        return False
+    else:
+        return True
